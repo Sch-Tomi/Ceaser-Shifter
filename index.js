324e6902f2eeab5bedfd3359792cf5295eceedf0
SchTomi <sch.tom93@gmail.com>
2017-05-30 23:44:15 +0200

    Add nodemon

--- index.js
+++ index.js
@@ -1,10 +1,16 @@
 const express = require('express')
 const app = express()
 
-app.use(express.static(__dirname + '/View'));
+app.use(express.static('public'))
+app.use('/scripts', express.static(__dirname + '/node_modules/bootstrap/dist/'));
+app.use('/css', express.static(__dirname + '/node_modules/bootstrap/dist/css/'));
+app.use('/fonts', express.static(__dirname + '/node_modules/bootstrap/fonts/'));
+
+app.use('/vue', express.static(__dirname + '/node_modules/vue/dist'));
+app.use('/vue-resource', express.static(__dirname + 'node_modules/vue-resource/dist/'));
 
 app.get('/', function(req, res) {
-    res.sendFile(__dirname + '/view/index.html');
+    res.sendFile(__dirname + '/public/index.html');
 
 });
 
