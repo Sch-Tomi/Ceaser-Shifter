6b5dd78ee03722d59c14bc5228851f5540526219
SchTomi <sch.tom93@gmail.com>
2017-05-31 12:24:07 +0200

    Client side encode/decode

--- public/js/app.js
+++ public/js/app.js
@@ -3,7 +3,8 @@ var vue = new Vue({
     el: '#events',
 
     data: {
-        chiperDict: []
+        abc: ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'],
+        chiperDict: [],
     },
 
     methods: {
@@ -23,7 +24,36 @@ var vue = new Vue({
                     document.querySelector("#key").parentNode.className += " has-error"
                     console.log(error);
                 });
-        }
+        },
 
+        code: function() {
+            let text = document.querySelector("#inputText").value;
+            let decoded = ""
+
+            for (let letter of text) {
+                if (letter == " ") {
+                    decoded += " "
+                } else {
+                    decoded += this.chiperDict[this.abc.indexOf(letter)]
+                }
+            }
+            document.querySelector("#outputText").value = decoded;
+
+        },
+
+        decode: function() {
+            let text = document.querySelector("#inputText").value;
+            let encoded = ""
+
+            for (let letter of text) {
+                if (letter == " ") {
+                    encoded += " "
+                } else {
+                    encoded += this.abc[this.chiperDict.indexOf(letter)]
+                }
+            }
+            document.querySelector("#outputText").value = encoded;
+
+        }
     }
 });
